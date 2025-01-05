c116365eb05cddae5763b061ef593d4d7d3692cc
Tim Rowe <tim@tjsr.id.au>
2025-01-06 08:43:20 +1100

    Force exit if cloudflare config values aren't passed to env.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,17 @@
 import { defineConfig } from 'drizzle-kit';
 
+let missingEnv = false;
+['CLOUDFLARE_ACCOUNT_ID', 'CLOUDFLARE_DATABASE_ID', 'CLOUDFLARE_D1_TOKEN'].forEach((key) => {
+  if (process.env[key] === undefined) {
+    console.error(`${key} is not set.  Configure this in .env`);
+    missingEnv = true;
+  }
+});
+
+if (missingEnv) {
+  process.exit(1);
+}
+
 export default defineConfig({
   schema: './src/orm/drizzle/schema/index.ts',
   dialect: 'sqlite',
